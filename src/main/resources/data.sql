0240a03907adc6bbe4013a98907ca2742560977d
Bence <m.bence05@gmail.com>
2020-06-16 14:49:31 +0200

    pridanie dalsieho pouzivatela

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,10 +11,14 @@ INSERT INTO users (account_id, first_name, last_name, gender, balance, tel, is_d
 VALUES (3, 'Peter', 'Varga', 'Muž', 350.45, '+421915521884', FALSE, CURRENT_TIMESTAMP);
 INSERT INTO users (account_id, first_name, last_name, gender, balance, tel, is_deleted, created_at)
 VALUES (4, 'Michal', 'Veľký', 'Muž', 100.00, '+421911521835', FALSE, CURRENT_TIMESTAMP);
+INSERT INTO users (account_id, first_name, last_name, gender, balance, tel, is_deleted, created_at)
+VALUES (5, 'Lukáš', 'Trnka', 'Muž', 80.00, '+421453142155', FALSE, CURRENT_TIMESTAMP);
 
 INSERT INTO places (user_id, country, region, district, place, street, zip, code, is_deleted, created_at)
 VALUES (1, 'Slovakia', 'Nitriansky kraj', 'Nové Zámky', 'Nesvady', 'Nová 25', '94651', 'sk', FALSE, CURRENT_TIMESTAMP);
 INSERT INTO places (user_id, country, region, district, place, street, zip, code, is_deleted, created_at)
-VALUES (2, 'Slovakia', 'Banská Bystrica', 'Poltár', 'Bádice', 'Obchodná 11', '95146', 'sk', FALSE, CURRENT_TIMESTAMP);
+VALUES (2, 'Slovakia', 'Banskobystrický kraj', 'Poltár', 'Bádice', 'Obchodná 11', '95146', 'sk', FALSE, CURRENT_TIMESTAMP);
 INSERT INTO places (user_id, country, region, district, place, street, zip, code, is_deleted, created_at)
-VALUES (3, 'Slovakia', 'Nitra', 'Bratislavský kraj', 'Bratislava', 'Knižná 33', '81107', 'sk', FALSE, CURRENT_TIMESTAMP);
+VALUES (3, 'Slovakia', 'Bratislavský kraj', 'Pezinok', 'Báčovce', 'Knižná 33', '96268', 'sk', FALSE, CURRENT_TIMESTAMP);
+INSERT INTO places (user_id, country, region, district, place, street, zip, code, is_deleted, created_at)
+VALUES (4, 'Slovakia', 'Banskobystrický kraj', 'Banská Bystrica', 'Banská Bystrica', 'Hlvná 1', '97401', 'sk', FALSE, CURRENT_TIMESTAMP);
